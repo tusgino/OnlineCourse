d6e0894223bd1b43d6ce5f882007996aaeb823b6
DangHuan2k3 <huan.bk.0412@gmail.com>
2023-04-20 21:50:29 +0700

    First Commit

--- Common/Req/Lesson/LessonReq.cs
+++ Common/Req/Lesson/LessonReq.cs
@@ -8,5 +8,11 @@ namespace Common.Req.Lesson
 {
     public class LessonReq
     {
+        public Guid? IdChapter { get; set; }
+        public string? Description { get; set; }
+        public int? Index { get; set; }
+        public string? Video { get; set; }
+        public double? Duration { get; set; }
+        public string? Title { get; set; }
     }
 }
