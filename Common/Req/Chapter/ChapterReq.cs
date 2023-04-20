230b199cab29e573f83ffe728adc7c188003475d
DangHuan2k3 <huan.bk.0412@gmail.com>
2023-04-20 21:51:38 +0700

    Post and Del Chapter

--- Common/Req/Chapter/ChapterReq.cs
+++ Common/Req/Chapter/ChapterReq.cs
@@ -8,7 +8,6 @@ namespace Common.Req.Chapter
 {
     public class ChapterReq
     {
-        public Guid IdChapter { get; set; }
         public string? Name { get; set; }
         public int? Index { get; set; }
         public Guid? IdCourse { get; set; }
