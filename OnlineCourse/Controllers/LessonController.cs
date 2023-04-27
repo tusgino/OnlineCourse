950e11be33eea5667424ba3ce3be93b4fbfbde4e
Tugino <nguyendinhtu.job@gmail.com>
2023-04-27 08:21:53 +0700

    fix Lesson/ChangeStatus --fix

--- OnlineCourse/Controllers/LessonController.cs
+++ OnlineCourse/Controllers/LessonController.cs
@@ -1,4 +1,5 @@
 ﻿using BLL;
+using Common.Req.Lesson;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -26,9 +27,10 @@ namespace OnlineCourse.Controllers
         }
 
         [HttpPost("change-status")]
-        public IActionResult ChangeStatus(Guid idUser, Guid idLesson)
+        [Authorize]
+        public IActionResult ChangeStatus([FromBody] ChangeStatusReq changeStatusReq)
         {
-            var rsp = _lessonSvc.ChangeStatus(idUser, idLesson);
+            var rsp = _lessonSvc.ChangeStatus(changeStatusReq.IdUser, changeStatusReq.IdLesson);
             if (rsp.Success)
             {
                 return Ok(rsp);
