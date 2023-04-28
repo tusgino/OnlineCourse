e8f0db5b68a8a46695bdd1e497bfbbf701b33ef7
Huy <baohuyvongoc1@gmail.com>
2023-04-28 09:53:55 +0700

    fix bug system revenue

--- DAL/TradeDetailRep.cs
+++ DAL/TradeDetailRep.cs
@@ -83,15 +83,9 @@ namespace DAL
             {
                 return context.TradeDetails.Where(trade => trade.TypeOfTrade == 1)
                                            .OrderBy(trade => trade.DateOfTrade)
-                                           .GroupBy(trade => trade.DateOfTrade.Value.Year, trade => trade.DateOfTrade.Value.Month)
+                                           .GroupBy(trade => trade.DateOfTrade.Value.Month)
                                            .Select(group => new { Month = group.Key, Revenue = group.Sum(trade => Convert.ToInt64(trade.Balance)) })
                                            .ToList<object>();
-
-
-
-
-
-
             }
         }
     }
